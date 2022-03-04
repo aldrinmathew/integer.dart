59966be2870225906dd1043b198ae6f659395f03
Aldrin Mathew <admin@aldrinmathew.com>
2022-03-04 23:01:47 +0530

    Updated `i4` datatype
    - Common operators have been moved to `integer`
    
    - Operators are now compatible with multiple datatypes. In most cases,
      int, double and `integer` variants are compatible with the operators
    
    - Added documentation for elements

--- lib/types/i4.dart
+++ lib/types/i4.dart
@@ -5,18 +5,78 @@ part of integer;
 /// 4-bit Signed Integer
 ///
 /// `-8` to `7`
-class i4 extends SignedInteger {
-  i4(int value) : super(value: value.int4);
+///
+/// The `i4` type is contagious. Most operations on `i4` will return `i4`
+///  values
+class i4 extends signed {
+  /// 4-bit Signed Integer
+  ///
+  /// `-8` to `7`
+  i4(int value) : super(value: value.toSigned(4));
 
-  i4.integer(integer val) : super(value: val.value.int4);
+  /// 4-bit Signed Integer
+  ///
+  /// `-8` to `7`
+  ///
+  /// Create `i4` from another `integer` value
+  i4.integer(integer val) : super(value: val.value.toSigned(4));
 
-  factory i4.fromEnvironment(int bits, String name, {int defaultValue = 0}) {
-    return i4(int.fromEnvironment(name, defaultValue: defaultValue.int4));
+  /// 4-bit Signed Integer
+  ///
+  /// `-8` to `7`
+  ///
+  /// Returns the integer value of the given environment declaration [name].
+  ///
+  /// The result is the same as would be returned by:
+  /// ```dart template:expression
+  /// i4(int.tryParse(const String.fromEnvironment(name, defaultValue: ""))
+  ///     ?? defaultValue)
+  /// ```
+  /// Example:
+  /// ```dart
+  /// i4.fromEnvironment("defaultPort", defaultValue: 80)
+  /// ```
+  factory i4.fromEnvironment(String name, {int defaultValue = 0}) {
+    return i4(
+      int.fromEnvironment(
+        name,
+        defaultValue: defaultValue.toSigned(4),
+      ),
+    );
   }
 
-  i4.parse(String source) : super(value: int.parse(source).int4);
+  /// 4-bit Signed Integer
+  ///
+  /// `-8` to `7`
+  ///
+  /// Parse [source] as a, possibly signed, integer literal and return its value.
+  ///
+  /// The [source] must be a non-empty sequence of base-[radix] digits,
+  /// optionally prefixed with a minus or plus sign ('-' or '+').
+  ///
+  /// The [radix] must be in the range 2..36. The digits used are
+  /// first the decimal digits 0..9, and then the letters 'a'..'z' with
+  /// values 10 through 35. Also accepts upper-case letters with the same
+  /// values as the lower-case ones.
+  ///
+  /// If no [radix] is given then it defaults to 10. In this case, the [source]
+  /// digits may also start with `0x`, in which case the number is interpreted
+  /// as a hexadecimal integer literal.
+  i4.parse(
+    String source, {
+    int? radix,
+  }) : super(
+          value: int.parse(
+            source,
+            radix: radix,
+          ).toSigned(4),
+        );
 
-  static i4? tryParse(int bits, String source) {
+  /// Parse [source] as a, signed integer literal.
+  ///
+  /// Like [parse] except that this function returns `null` where a
+  /// similar call to [parse] would throw a [FormatException].
+  static i4? tryParse(String source) {
     int? val = int.tryParse(source);
     if (val != null) {
       return i4(val);
@@ -24,76 +84,269 @@ class i4 extends SignedInteger {
     return null;
   }
 
+  /// Multiplies this integer by `other`.
+  ///
+  /// If the `other` number is `double`, the value is truncated after
+  ///  multiplication
+  ///
+  /// Returns `i4`
   @override
-  i4 operator *(integer other) => i4(value * other.value);
+  i4 operator *(dynamic other) {
+    if (other is integer) {
+      return i4(value * other.value);
+    } else if (other is int) {
+      return i4(value * other);
+    } else if (other is double) {
+      return i4((value * other).truncate());
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Adds `other` to this number.
+  ///
+  /// If the `other` number is `double`, the value is truncated after
+  ///  addition
+  ///
+  /// Returns `i4`
   @override
-  i4 operator +(integer other) => i4(value + other.value);
+  i4 operator +(dynamic other) {
+    if (other is integer) {
+      return i4(value + other.value);
+    } else if (other is int) {
+      return i4(value + other);
+    } else if (other is double) {
+      return i4((value + other).truncate());
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Subtracts `other` from this number.
+  ///
+  /// If the `other` number is `double`, the value is truncated after
+  ///  subtraction
+  ///
+  /// Returns `i4`
   @override
-  i4 operator -(integer other) => i4(value - other.value);
+  i4 operator -(dynamic other) {
+    if (other is integer) {
+      return i4(value - other.value);
+    } else if (other is int) {
+      return i4(value - other);
+    } else if (other is double) {
+      return i4((value - other).truncate());
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Truncating division operator.
+  ///
+  /// Performs truncating division of this number by [other].
+  /// Truncating division is division where a fractional result
+  /// is converted to an integer by rounding towards zero.
+  ///
+  /// If both operands are [int]s, then [other] must not be zero.
+  /// Then `a ~/ b` corresponds to `a.remainder(b)`
+  /// such that `a == (a ~/ b) * b + a.remainder(b)`.
+  ///
+  /// `a ~/ b` is equivalent to `(a / b).truncate()`.
+  /// This means that the intermediate result of the double division
+  /// must be a finite integer (not an infinity or [double.nan]).
+  ///
+  /// Returns `i4`
   @override
-  double operator /(integer other) => value / other.value;
+  i4 operator ~/(dynamic other) {
+    if (other is integer) {
+      return i4(value ~/ other.value);
+    } else if (other is int) {
+      return i4(value ~/ other);
+    } else if (other is double) {
+      return i4(value ~/ other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Bit-wise and operator.
+  ///
+  /// Treating both `this` and [other] as sufficiently large two's component
+  /// integers, the result is a number with only the bits set that are set in
+  /// both `this` and [other]
+  ///
+  /// If both operands are negative, the result is negative, otherwise
+  /// the result is non-negative.
+  ///
+  /// Returns `i4`
   @override
-  i4 operator ~/(integer other) => i4(value ~/ other.value);
+  i4 operator &(dynamic other) {
+    if (other is integer) {
+      return i4(value & other.value);
+    } else if (other is int) {
+      return i4(value & other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Bit-wise or operator.
+  ///
+  /// Treating both `this` and [other] as sufficiently large two's component
+  /// integers, the result is a number with the bits set that are set in either
+  /// of `this` and [other]
+  ///
+  /// If both operands are non-negative, the result is non-negative,
+  /// otherwise the result is negative.
+  ///
+  /// Returns `i4`
   @override
-  i4 operator &(integer other) => i4(value & other.value);
+  i4 operator |(dynamic other) {
+    if (other is integer) {
+      return i4(value | other.value);
+    } else if (other is int) {
+      return i4(value | other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Bit-wise exclusive-or operator.
+  ///
+  /// Treating both `this` and [other] as sufficiently large two's component
+  /// integers, the result is a number with the bits set that are set in one,
+  /// but not both, of `this` and [other]
+  ///
+  /// If the operands have the same sign, the result is non-negative,
+  /// otherwise the result is negative.
+  ///
+  /// Returns `i4`
   @override
-  i4 operator |(integer other) => i4(value | other.value);
+  i4 operator ^(dynamic other) {
+    if (other is integer) {
+      return i4(value ^ other.value);
+    } else if (other is int) {
+      return i4(value ^ other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
-  @override
-  i4 operator ^(integer other) => i4(value ^ other.value);
-
+  /// The bit-wise negate operator.
+  ///
+  /// Treating `this` as a sufficiently large two's component integer,
+  /// the result is a number with the opposite bits set.
+  ///
+  /// This maps any integer `x` to `-x - 1`.
+  ///
+  /// Returns `i4`
   @override
   i4 operator ~() => i4(~value);
 
+  /// Shift the bits of this integer to the left by [shiftAmount].
+  ///
+  /// Shifting to the left makes the number larger, effectively multiplying
+  /// the number by `pow(2, shiftIndex)`.
+  ///
+  /// There is no limit on the size of the result. It may be relevant to
+  /// limit intermediate values by using the "and" operator with a suitable
+  /// mask.
+  ///
+  /// It is an error if [shiftAmount] is negative.
+  ///
+  /// Returns `i4`
   @override
-  i4 operator <<(integer other) => i4(value << other.value);
+  i4 operator <<(dynamic other) {
+    if (other is integer) {
+      return i4(value << other.value);
+    } else if (other is int) {
+      return i4(value << other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Shift the bits of this integer to the right by [shiftAmount].
+  ///
+  /// Shifting to the right makes the number smaller and drops the least
+  /// significant bits, effectively doing an integer division by
+  /// `pow(2, shiftIndex)`.
+  ///
+  /// It is an error if [shiftAmount] is negative.
+  ///
+  /// Returns `i4`
   @override
-  i4 operator >>(integer other) => i4(value >> other.value);
+  i4 operator >>(dynamic other) {
+    if (other is integer) {
+      return i4(value >> other.value);
+    } else if (other is int) {
+      return i4(value >> other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Bitwise unsigned right shift by [shiftAmount] bits.
+  ///
+  /// The least significant [shiftAmount] bits are dropped,
+  /// the remaining bits (if any) are shifted down,
+  /// and zero-bits are shifted in as the new most significant bits.
+  ///
+  /// The [shiftAmount] must be non-negative.
+  ///
+  /// Returns `i4`
   @override
-  i4 operator >>>(integer other) => i4(value >>> other.value);
+  i4 operator >>>(dynamic other) {
+    if (other is integer) {
+      return i4(value >>> other.value);
+    } else if (other is int) {
+      return i4(value >>> other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Returns this integer to the power of [exponent] modulo [modulus].
+  ///
+  /// The [exponent] must be non-negative and [modulus] must be positive.
   @override
-  i4 modPow(integer exponent, integer modulus) =>
+  i4 modPow(unsigned exponent, unsigned modulus) =>
       i4(value.modPow(exponent.value, modulus.value));
 
+  /// Returns the modular multiplicative inverse of this integer
+  /// modulo [modulus].
+  ///
+  /// The [modulus] must be positive.
+  ///
+  /// It is an error if no modular inverse exists.
   @override
-  i4 modInverse(integer modulus) => i4(value.modInverse(modulus.value));
+  i4 modInverse(unsigned modulus) => i4(value.modInverse(modulus.value));
 
+  /// Returns the greatest common divisor of this integer and [other].
+  ///
+  /// If either number is non-zero, the result is the numerically greatest
+  /// integer dividing both `this` and `other`.
+  ///
+  /// The greatest common divisor is independent of the order,
+  /// so `x.gcd(y)` is  always the same as `y.gcd(x)`.
+  ///
+  /// For any integer `x`, `x.gcd(x)` is `x.abs()`.
+  ///
+  /// If both `this` and `other` is zero, the result is also zero.
   @override
   i4 gcd(integer other) => i4(value.gcd(other.value));
 
-  @override
-  bool get isEven => value.isEven;
-
-  @override
-  bool get isOdd => value.isOdd;
-
-  @override
-  ux toUnsigned(UnsignedInteger bits) => ux(bits: bits.value, value: value);
-
+  /// Return the negative value of this integer.
+  ///
+  /// The result of negating an integer always has the opposite sign, except
+  /// for zero, which is its own negation.
   @override
   i4 operator -() => i4(-value);
 
+  /// Returns the absolute value of this integer.
+  ///
+  /// For any integer `value`,
+  /// the result is the same as `value < 0 ? -value : value`.
   @override
   u4 abs() => u4(value.abs());
-
-  @override
-  i4 round() => i4(value.round());
-
-  @override
-  i4 floor() => i4(value.floor());
-
-  @override
-  i4 ceil() => i4(value.ceil());
-
-  @override
-  i4 truncate() => i4(value.truncate());
 }
