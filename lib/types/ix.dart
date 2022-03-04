afeadb5b1aab07f0603ac1d0d83c40bdc0f91c1b
Aldrin Mathew <admin@aldrinmathew.com>
2022-03-04 23:03:11 +0530

    Updated `ix` datatype
    - Common operators have been moved to `integer`
    
    - Operators are now compatible with multiple datatypes. In most cases,
      int, double and `integer` variants are compatible with the operators
    
    - Added documentation for elements

--- lib/types/ix.dart
+++ lib/types/ix.dart
@@ -3,37 +3,88 @@
 part of integer;
 
 /// Signed Integer with custom bit-width
-class ix extends SignedInteger {
+///
+/// `-2 ^ (bitWidth / 2)` to `(2 ^ (bitWidth / 2)) - 1`
+class ix extends signed {
   int _bits;
 
   /// Bitwidth of this integer. This is same as [bitLength]
   int get bits => _bits;
 
+  /// Signed Integer with custom bit-width
+  ///
+  /// `-2 ^ (bitWidth / 2)` to `(2 ^ (bitWidth / 2)) - 1`
   ix({
     required int bits,
     required int value,
-  })  : _bits = bits,
-        super(value: value.intX(bits));
+  })  : _bits = bits.clamp(2, 64),
+        super(value: value.toSigned(bits.clamp(2, 64)));
 
+  /// Signed Integer with custom bit-width
+  ///
+  /// `-2 ^ (bitWidth / 2)` to `(2 ^ (bitWidth / 2)) - 1`
+  ///
+  /// Create `ix` from another `integer` value
   ix.integer(integer val, {required int bits})
-      : _bits = bits,
-        super(value: val.value.intX(bits));
+      : _bits = bits.clamp(2, 64),
+        super(value: val.value.toSigned(bits.clamp(2, 64)));
 
+  /// Signed Integer with custom bit-width
+  ///
+  /// `-2 ^ (bitWidth / 2)` to `(2 ^ (bitWidth / 2)) - 1`
+  ///
+  /// Returns the integer value of the given environment declaration [name].
+  ///
+  /// The result is the same as would be returned by:
+  /// ```dart template:expression
+  /// ix(int.tryParse(const String.fromEnvironment(name, defaultValue: ""))
+  ///     ?? defaultValue)
+  /// ```
+  /// Example:
+  /// ```dart
+  /// ix.fromEnvironment("defaultPort", defaultValue: 80)
+  /// ```
   factory ix.fromEnvironment(
     String name, {
     required int bits,
     int defaultValue = 0,
   }) {
     return ix(
-      bits: bits,
-      value: int.fromEnvironment(name, defaultValue: defaultValue.intX(bits)),
+      bits: bits.clamp(2, 64),
+      value: int.fromEnvironment(
+        name,
+        defaultValue: defaultValue.toSigned(bits.clamp(2, 64)),
+      ),
     );
   }
 
-  ix.parse(String source, {required int bits})
-      : _bits = bits,
-        super(value: int.parse(source).intX(bits));
+  /// Signed Integer with custom bit-width
+  ///
+  /// `-2 ^ (bitWidth / 2)` to `(2 ^ (bitWidth / 2)) - 1`
+  ///
+  /// Parse [source] as a, possibly signed, integer literal and return its value.
+  ///
+  /// The [source] must be a non-empty sequence of base-[radix] digits,
+  /// optionally prefixed with a minus or plus sign ('-' or '+').
+  ///
+  /// The [radix] must be in the range 2..36. The digits used are
+  /// first the decimal digits 0..9, and then the letters 'a'..'z' with
+  /// values 10 through 35. Also accepts upper-case letters with the same
+  /// values as the lower-case ones.
+  ///
+  /// If no [radix] is given then it defaults to 10. In this case, the [source]
+  /// digits may also start with `0x`, in which case the number is interpreted
+  /// as a hexadecimal integer literal.
+  ix.parse(
+    String source, {
+    required int bits,
+  })  : _bits = bits.clamp(2, 64),
+        super(value: int.parse(source).toSigned(bits.clamp(2, 64)));
 
+  /// Parse [source] as a, signed integer literal.
+  ///
+  /// Like [parse] except that this function returns `null` where a
+  /// similar call to [parse] would throw a [FormatException].
   static ix? tryParse(int bits, String source) {
     int? val = int.tryParse(source);
     if (val != null) {
@@ -42,81 +93,278 @@ class ix extends SignedInteger {
     return null;
   }
 
-  ix copy({int? bits, int? value}) =>
-      ix(bits: bits ?? _bits, value: value ?? _value);
+  /// Copy `ix` with the provided arguments
+  ix copy({int? bits, int? value}) => ix(
+        bits: bits ?? _bits,
+        value: value ?? this.value,
+      );
 
+  /// Create another `ix` with the provided value
   ix ofSameBit(int value) => ix(bits: bits, value: value);
 
+  /// Multiplies this integer by `other`.
+  ///
+  /// If the `other` number is `double`, the value is truncated after
+  ///  multiplication
+  ///
+  /// Returns `ix`
   @override
-  ix operator *(integer other) => ofSameBit(value * other.value);
+  ix operator *(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value * other.value);
+    } else if (other is int) {
+      return ofSameBit(value * other);
+    } else if (other is double) {
+      return ofSameBit((value * other).truncate());
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Adds `other` to this number.
+  ///
+  /// If the `other` number is `double`, the value is truncated after
+  ///  addition
+  ///
+  /// Returns `ix`
   @override
-  ix operator +(integer other) => ofSameBit(value + other.value);
+  ix operator +(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value + other.value);
+    } else if (other is int) {
+      return ofSameBit(value + other);
+    } else if (other is double) {
+      return ofSameBit((value + other).truncate());
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Subtracts `other` from this number.
+  ///
+  /// If the `other` number is `double`, the value is truncated after
+  ///  subtraction
+  ///
+  /// Returns `ix`
   @override
-  ix operator -(integer other) => ofSameBit(value - other.value);
+  ix operator -(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value - other.value);
+    } else if (other is int) {
+      return ofSameBit(value - other);
+    } else if (other is double) {
+      return ofSameBit((value - other).truncate());
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Truncating division operator.
+  ///
+  /// Performs truncating division of this number by [other].
+  /// Truncating division is division where a fractional result
+  /// is converted to an integer by rounding towards zero.
+  ///
+  /// If both operands are [int]s, then [other] must not be zero.
+  /// Then `a ~/ b` corresponds to `a.remainder(b)`
+  /// such that `a == (a ~/ b) * b + a.remainder(b)`.
+  ///
+  /// `a ~/ b` is equivalent to `(a / b).truncate()`.
+  /// This means that the intermediate result of the double division
+  /// must be a finite integer (not an infinity or [double.nan]).
+  ///
+  /// Returns `ix`
   @override
-  double operator /(integer other) => value / other.value;
+  ix operator ~/(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value ~/ other.value);
+    } else if (other is int) {
+      return ofSameBit(value ~/ other);
+    } else if (other is double) {
+      return ofSameBit(value ~/ other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Bit-wise and operator.
+  ///
+  /// Treating both `this` and [other] as sufficiently large two's component
+  /// integers, the result is a number with only the bits set that are set in
+  /// both `this` and [other]
+  ///
+  /// If both operands are negative, the result is negative, otherwise
+  /// the result is non-negative.
+  ///
+  /// Returns `ix`
   @override
-  ix operator ~/(integer other) => ofSameBit(value ~/ other.value);
+  ix operator &(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value & other.value);
+    } else if (other is int) {
+      return ofSameBit(value & other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Bit-wise or operator.
+  ///
+  /// Treating both `this` and [other] as sufficiently large two's component
+  /// integers, the result is a number with the bits set that are set in either
+  /// of `this` and [other]
+  ///
+  /// If both operands are non-negative, the result is non-negative,
+  /// otherwise the result is negative.
+  ///
+  /// Returns `ix`
   @override
-  ix operator &(integer other) => ofSameBit(value & other.value);
+  ix operator |(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value | other.value);
+    } else if (other is int) {
+      return ofSameBit(value | other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Bit-wise exclusive-or operator.
+  ///
+  /// Treating both `this` and [other] as sufficiently large two's component
+  /// integers, the result is a number with the bits set that are set in one,
+  /// but not both, of `this` and [other]
+  ///
+  /// If the operands have the same sign, the result is non-negative,
+  /// otherwise the result is negative.
+  ///
+  /// Returns `ix`
   @override
-  ix operator |(integer other) => ofSameBit(value | other.value);
+  ix operator ^(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value ^ other.value);
+    } else if (other is int) {
+      return ofSameBit(value ^ other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
-  @override
-  ix operator ^(integer other) => ofSameBit(value ^ other.value);
-
+  /// The bit-wise negate operator.
+  ///
+  /// Treating `this` as a sufficiently large two's component integer,
+  /// the result is a number with the opposite bits set.
+  ///
+  /// This maps any integer `x` to `-x - 1`.
+  ///
+  /// Returns `ix`
   @override
   ix operator ~() => ofSameBit(~value);
 
+  /// Shift the bits of this integer to the left by [shiftAmount].
+  ///
+  /// Shifting to the left makes the number larger, effectively multiplying
+  /// the number by `pow(2, shiftIndex)`.
+  ///
+  /// There is no limit on the size of the result. It may be relevant to
+  /// limit intermediate values by using the "and" operator with a suitable
+  /// mask.
+  ///
+  /// It is an error if [shiftAmount] is negative.
+  ///
+  /// Returns `ix`
   @override
-  ix operator <<(integer other) => ofSameBit(value << other.value);
+  ix operator <<(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value << other.value);
+    } else if (other is int) {
+      return ofSameBit(value << other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Shift the bits of this integer to the right by [shiftAmount].
+  ///
+  /// Shifting to the right makes the number smaller and drops the least
+  /// significant bits, effectively doing an integer division by
+  /// `pow(2, shiftIndex)`.
+  ///
+  /// It is an error if [shiftAmount] is negative.
+  ///
+  /// Returns `ix`
   @override
-  ix operator >>(integer other) => ofSameBit(value >> other.value);
+  ix operator >>(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value >> other.value);
+    } else if (other is int) {
+      return ofSameBit(value >> other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Bitwise unsigned right shift by [shiftAmount] bits.
+  ///
+  /// The least significant [shiftAmount] bits are dropped,
+  /// the remaining bits (if any) are shifted down,
+  /// and zero-bits are shifted in as the new most significant bits.
+  ///
+  /// The [shiftAmount] must be non-negative.
+  ///
+  /// Returns `ix`
   @override
-  ix operator >>>(integer other) => ofSameBit(value >>> other.value);
+  ix operator >>>(dynamic other) {
+    if (other is integer) {
+      return ofSameBit(value >>> other.value);
+    } else if (other is int) {
+      return ofSameBit(value >>> other);
+    } else {
+      throw Exception('Invalid type for operand: ${dynamic.runtimeType}');
+    }
+  }
 
+  /// Returns this integer to the power of [exponent] modulo [modulus].
+  ///
+  /// The [exponent] must be non-negative and [modulus] must be positive.
   @override
-  ix modPow(integer exponent, integer modulus) =>
+  ix modPow(unsigned exponent, unsigned modulus) =>
       ofSameBit(value.modPow(exponent.value, modulus.value));
 
+  /// Returns the modular multiplicative inverse of this integer
+  /// modulo [modulus].
+  ///
+  /// The [modulus] must be positive.
+  ///
+  /// It is an error if no modular inverse exists.
   @override
-  ix modInverse(integer modulus) => ofSameBit(value.modInverse(modulus.value));
+  ix modInverse(unsigned modulus) => ofSameBit(value.modInverse(modulus.value));
 
+  /// Returns the greatest common divisor of this integer and [other].
+  ///
+  /// If either number is non-zero, the result is the numerically greatest
+  /// integer dividing both `this` and `other`.
+  ///
+  /// The greatest common divisor is independent of the order,
+  /// so `x.gcd(y)` is  always the same as `y.gcd(x)`.
+  ///
+  /// For any integer `x`, `x.gcd(x)` is `x.abs()`.
+  ///
+  /// If both `this` and `other` is zero, the result is also zero.
   @override
   ix gcd(integer other) => ofSameBit(value.gcd(other.value));
 
-  @override
-  bool get isEven => value.isEven;
-
-  @override
-  bool get isOdd => value.isOdd;
-
-  @override
-  ux toUnsigned(UnsignedInteger bits) => ux(bits: bits.value, value: value);
-
+  /// Return the negative value of this integer.
+  ///
+  /// The result of negating an integer always has the opposite sign, except
+  /// for zero, which is its own negation.
   @override
   ix operator -() => ofSameBit(-value);
 
+  /// Returns the absolute value of this integer.
+  ///
+  /// For any integer `value`,
+  /// the result is the same as `value < 0 ? -value : value`.
   @override
-  ux abs() => ux(bits: (bits ~/ 2), value: value.abs());
-
-  @override
-  ix round() => ofSameBit(value.round());
-
-  @override
-  ix floor() => ofSameBit(value.floor());
-
-  @override
-  ix ceil() => ofSameBit(value.ceil());
-
-  @override
-  ix truncate() => ofSameBit(value.truncate());
+  ux abs() => ux(bits: _bits ~/ 2, value: value.abs());
 }
